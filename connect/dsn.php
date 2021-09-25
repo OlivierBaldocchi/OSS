b5431af3f39ad65520c44195f60147cc26bf4f38
Olivier Baldocchi <olivierbaldocchi@gmail.com>
2021-09-25 18:32:21 +0200

    modif connect

--- connect/dsn.php
+++ connect/dsn.php
@@ -1,10 +1,11 @@
 <?php
 
-$host = 'localhost';
-        $dbname = 'OSS';
-        $username = 'postgres';
-        $password = 'bddcinema';
-        $dsn = "pgsql:host=$host;port=5432;dbname=$dbname;user=$username;password=$password";
+$host = 'ec2-18-209-153-180.compute-1.amazonaws.com';
+$dbname = 'd364clio7jt1g6';
+$username = 'efdekfahnguxsi';
+$password = '2dd8040f754fea14b9f4ffb6a6c1af2f24e4a69a8939defbbd5a0c5ddbddce53';
+
+$dsn = "pgsql:host=$host;port=5432;dbname=$dbname;user=$username;password=$password";
 
 try {
     $conn = new PDO($dsn);
