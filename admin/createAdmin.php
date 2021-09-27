b974647aa0f364d23ea558ccf4c94a9fd9029633
Olivier Baldocchi <olivierbaldocchi@gmail.com>
2021-09-27 17:39:42 +0200

    ok

--- admin/createAdmin.php
+++ admin/createAdmin.php
@@ -1,24 +1,28 @@
 <?php
 require_once '../base.html';?> 
 
-<body class='yellow'>
+<body>
+    <div class='yellow'>
+        <?php
+        include '../vue/buttonLogOut.php';
+        include '../vue/buttonBack.php';
+        require_once '../connect/dsn.php';
+        
+        $statement = $conn->prepare('INSERT INTO admins (nom, prenom, mail, mot_de_passe, date_creation) 
+                                    VALUES (:nom, :prenom, :email, :password, :date_creation)');
+        $statement->bindValue(':nom', $_POST['nom']);
+        $statement->bindValue(':prenom', $_POST['prenom']);
+        $statement->bindValue(':email', $_POST['mail']);
+        $statement->bindValue(':password', password_hash($_POST['password'], PASSWORD_BCRYPT));
+        $statement->bindValue(':date_creation', $_POST['date_creation']);
+        
+        if ($statement->execute()) {
+            echo 'Bravo!' . '<br>' . 'L\'administrateur ' . '<br>' . $_POST['prenom'] . ' ' . $_POST['nom'] . '<br>' . ' a été créé';
+        } else {
+            echo 'Impossible de créer l\'administrateur';
+        }
+        ?>    
+    </div>
 
-    <?php
-    include '../vue/buttonLogOut.php';
-    include '../vue/buttonBack.php';
-    require_once '../connect/dsn.php';
     
-    $statement = $conn->prepare('INSERT INTO admins(nom, prenom, mail, mot_de_passe, date_creation) VALUES (:nom, :prenom, :email, :password, :date_creation)');
-    $statement->bindValue(':nom', $_POST['nom']);
-    $statement->bindValue(':prenom', $_POST['prenom']);
-    $statement->bindValue(':email', $_POST['mail']);
-    $statement->bindValue(':password', password_hash($_POST['password'], PASSWORD_BCRYPT));
-    $statement->bindValue(':date_creation', $_POST['date_creation']);
-    
-    if ($statement->execute()) {
-        echo 'Bravo!' . '<br>' . 'L\'administrateur ' . '<br>' . $_POST['prenom'] . ' ' . $_POST['nom'] . '<br>' . ' a été créé';
-    } else {
-        echo 'Impossible de créer l\'administrateur';
-    }
-    ?>   
 </body>
