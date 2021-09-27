b69a8be1c670483191128c2e3a9be0f97f47f55b
Olivier Baldocchi <olivierbaldocchi@gmail.com>
2021-09-27 17:53:49 +0200

    ok

--- agent/createAgent.php
+++ agent/createAgent.php
@@ -1,21 +1,28 @@
 <?php
 require_once '../base.html';?> 
 
-<body class='yellow'>
+<body class='yellow'> 
 
     <?php
     include '../vue/buttonLogOut.php';
     include '../vue/buttonBack.php';
     require_once '../connect/dsn.php';
+
+    $nom = $_POST['nom'];   
+    $prenom = $_POST['prenom']; 
+    $date = $_POST['date_de_naissance'];
+    $pays = $_POST['pays'];
+    $specialite = $_POST['specialite']; 
+    $code = $_POST['nom_de_code']; 
     
     $statement = $conn->prepare('INSERT INTO agents(nom, prenom, date_de_naissance, nationalite, specialite, nom_de_code) 
                                     VALUES (:nom, :prenom, :birth, :pays, :specialite, :code)');
-    $statement->bindValue(':nom', $_POST['nom']);
-    $statement->bindValue(':prenom', $_POST['prenom']);
-    $statement->bindValue(':birth', $_POST['date_de_naissance']);
-    $statement->bindValue(':pays', $_POST['pays']);
-    $statement->bindValue(':specialite', $_POST['specialite']);
-    $statement->bindValue(':code', $_POST['nom_de_code']);
+    $statement->bindValue(':nom', $nom);
+    $statement->bindValue(':prenom', $prenom);
+    $statement->bindValue(':birth', $date);
+    $statement->bindValue(':pays', $pays);
+    $statement->bindValue(':specialite', $pecialite);
+    $statement->bindValue(':code', $code);
     
     if ($statement->execute()) {
         echo 'Bravo!' . '<br>' . 'L\'agent ' . '<br>' . $_POST['prenom'] . ' ' . $_POST['nom'] . '<br>' . ' a été créé';
