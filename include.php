b896907dc07abb3164e9d1a5e0697dc351625121
Olivier Baldocchi <olivierbaldocchi@gmail.com>
2021-09-27 18:52:39 +0200

    cancel

--- include.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include 'https://php-postgre-studi.herokuapp.com/vue/buttonLogOut.php';
-include 'https://php-postgre-studi.herokuapp.com/vue/buttonBack.php';
-require_once 'https://php-postgre-studi.herokuapp.com/connect/dsn.php';
-
-    
